39b4ab83ed674455c6fe7e46ffc667658789d41c
tyientyien3 <kunchikitaforp@gmail.com>
2020-06-22 03:07:23 +0000

    js/キャラクターランダム表示

--- js/main.js
+++ js/main.js
@@ -12,21 +12,40 @@
       '最も恐れられた'
     ];
 
-    var job = [
+    var jobs = [
       {name: '勇者', img: 'hero.gif'},
       {name: '魔法使い', img: 'wizard.gif'},
       {name: '武闘家', img: 'fighter.gif'}
     ];
 
-    var job = [
+    var types = [
       {name: 'その炎はすべてを焼き尽くす', img: 'bg-fire'},
       {name: 'その聖水で全てを浄化する', img: 'bg-water'},
       {name: 'その雷撃は万物の怒りを鎮める', img: 'bg-thunder'}
     ];
 
+    var msg;
+    var job;
+    var type;
+
+    var resultImg = document.getElementById('result_img');
+
     function getRandomElement(array) {
       return array[Math.floor(Math.random() * array.length)];
     }
+
+    function setTextContent(id, text) {
+      document.getElementById(id).textContent = text;
+    }
+
+    msg = getRandomElement(msgs);
+    job = getRandomElement(jobs);
+    type = getRandomElement(types);
+
+    setTextContent('result_msg', msg);
+    setTextContent('result_job', job.name);
+    resultImg.children[0].src = 'img/' + job.img;
+    setTextContent('result_type', type.name);
 
     cards.className = 'move';
   });
